1e0a25c451c81fcdb81c5e9677563f57c87e266d
jonathan.lazarou <jonathan.lazarou@temple.edu>
2023-03-02 11:27:17 -0500

    assigned adapter to spinner and recyclerView, and defined layout manager

--- app/src/main/java/edu/temple/inclassactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassactivity/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Spinner
 import android.widget.SpinnerAdapter
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
@@ -22,6 +23,10 @@ class MainActivity : AppCompatActivity() {
         typedArray.recycle()
 
         // Display images specified in imageArray in Spinner and RecyclerView
+        spinner?.adapter = CustomSpinnerAdapter(this, imageArray)
+
+        recyclerView?.adapter = CustomRecyclerAdapter(imageArray)
+        recyclerView?.layoutManager = LinearLayoutManager(this)
 
     }
 }
